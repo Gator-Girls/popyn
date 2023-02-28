9afdcf99d4f4c4df5f23bfd8b89b7361f4525036
vale3760 <vale3760@gmail.com>
2023-02-28 14:41:48 -0500

    modified files a bit to match our pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,32 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("src/layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      {
+        path: "/home",
+        component: () => import("src/pages/Home.vue"),
+        name: "Home",
+      },
+      {
+        path: "/profile",
+        component: () => import("src/pages/Profile.vue"),
+        name: "Profile",
+      },
+      {
+        path: "/search",
+        component: () => import("src/pages/SearchPage.vue"),
+        name: "Search",
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
