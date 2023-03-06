c6cbb01575ddf18321a8f00fb26f7c60c77a1ea0
dtrevino13 <d.trevino@ufl.edu>
2023-03-06 16:25:13 -0500

    update firebase config

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,7 +1,19 @@
-import { boot } from 'quasar/wrappers'
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async (/* { app, router, ... } */) => {
-  // something to do
-})
+const firebaseConfig = {
+  apiKey: "AIzaSyCdMwTwN5wmPFtBCnRGtE0OYixf-jJLK9k",
+  authDomain: "popyn-18d51.firebaseapp.com",
+  projectId: "popyn-18d51",
+  storageBucket: "popyn-18d51.appspot.com",
+  messagingSenderId: "699103021609",
+  appId: "1:699103021609:web:dbd96da9c777eaf541aa38",
+  measurementId: "G-9LSB6DSJJ8",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
+
+export default db;
