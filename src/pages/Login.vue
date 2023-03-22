0774230c782f880e11332d71bd1591f314e92499
Kelly <kdheng@ufl.edu>
2023-03-21 22:40:33 -0400

    Changes to logout and login

---
+++ src/pages/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="row justify-center">Logyn</h5>
+    <div class="q-gutter-md" style="max-width: 300px">
+      <q-input rounded outlined v-model="text" label="Username" />
+      <q-input rounded outlined v-model="text" label="Password" />
+    </div>
+
+    <div class="row justify-center">
+      <q-btn
+        rounded
+        color="primary"
+        text-color="black"
+        label="Log in"
+        to="/Profile"
+      ></q-btn>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageLogin",
+};
+</script>
