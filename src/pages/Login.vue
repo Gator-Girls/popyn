3d0ff7eb4f44bae46246af54b69a392d9863adcf
yelainelinares <yelainelinares@ufl.edu>
2023-03-21 22:52:17 -0400

    Spacing to login page

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,14 +1,17 @@
 <template>
   <q-page class="q-pa-lg">
     <h5 class="row justify-center">Logyn</h5>
-    <div class="q-gutter-md" style="max-width: 300px">
-      <q-input rounded outlined v-model="text" label="Username" />
-      <q-input rounded outlined v-model="text" label="Password" />
+    <div class="row justify-center">
+      <div class="col q-gutter-md" style="max-width: 400px">
+        <q-input rounded outlined v-model="text" label="Username" />
+        <q-input rounded outlined v-model="text" label="Password" />
+      </div>
     </div>
 
     <div class="row justify-center">
       <q-btn
         rounded
+        class="q-mt-md"
         color="primary"
         text-color="black"
         label="Log in"
