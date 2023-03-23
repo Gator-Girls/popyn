73663c74e8716e9c0c415885be80f61d414ff271
vale3760 <vale3760@gmail.com>
2023-03-23 00:38:02 -0400

    simplified a bit

--- src/main.js
+++ src/main.js
@@ -6,26 +6,6 @@ Vue.use(VueGoogleMaps, {
   },
 });
 
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCdMwTwN5wmPFtBCnRGtE0OYixf-jJLK9k",
-  authDomain: "popyn-18d51.firebaseapp.com",
-  projectId: "popyn-18d51",
-  storageBucket: "popyn-18d51.appspot.com",
-  messagingSenderId: "699103021609",
-  appId: "1:699103021609:web:dbd96da9c777eaf541aa38",
-  measurementId: "G-9LSB6DSJJ8",
-};
-
-firebase.initializeApp(firebaseConfig);
-const Firestore = firebase.firestore;
-export const db = Firebase();
-
-Vue.config.productionTip = false;
-
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
