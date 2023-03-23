b19657bcb8eb9d425d54fdfed1c56c1e2a998430
Kelly <kdheng@ufl.edu>
2023-03-23 16:01:34 -0400

    Progress on reviews page

--- src/pages/BackyardReviews.vue
+++ src/pages/BackyardReviews.vue
@@ -11,7 +11,27 @@
     <div class="column items-center">
       <q-list bordered separator>
         <q-item>
-          <q-item-section>Backyard box</q-item-section>
+          <q-item-section>
+            <div class="row">
+              <div class="col-8 text-left">
+                <div class="text-weight-bold">
+                  2. The Backyard at Boca Fiesta
+                </div>
+                <div class="q-pl-sm">
+                  <div>2.7 mi • Bar • $$</div>
+                  <div>Open 4:00pm - 2:00am</div>
+                  <div>★★★★☆ (38)</div>
+                </div>
+              </div>
+              <div class="col-4">
+                <img
+                  alt="Popyn logo"
+                  src="~assets/Popyn_-logos_transparent.ico"
+                  style="width: 88px; height: 88px"
+                />
+              </div>
+            </div>
+          </q-item-section>
         </q-item>
 
         <q-item>
@@ -21,10 +41,10 @@
           </q-item-section>
         </q-item>
 
-        <q-item clickable v-ripple>
+        <q-item>
           <q-item-section>
-            <q-item-label overline>OVERLINE</q-item-label>
-            <q-item-label>Review</q-item-label>
+            <q-item-label>Review 2</q-item-label>
+            <q-item-label caption>Caption</q-item-label>
           </q-item-section>
         </q-item>
       </q-list>
