f13a40c6c86efbf247a623f27a769e6c2bed5b73
vale3760 <vale3760@gmail.com>
2023-03-25 13:46:03 -0400

    finally one api call works

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,97 +1,28 @@
 <template>
-  <div>
-    <q-page class="q-pa-lg">
-      <q-input
-        placeholder="Whats Popyn?"
-        class="q-ma-md"
-        outlined
-        rounded
-        dense
-      >
-        <template v-slot:prepend>
-          <q-icon name="search" />
-        </template>
-      </q-input>
-      <q-btn icon="location_on" @click="findLocation" />
-    </q-page>
-
-    <section id="map"></section>
-  </div>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body1">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt similique
+        molestias dolore, ab libero eius? Voluptatum quaerat architecto ex
+        blanditiis, perspiciatis dolor quisquam labore fuga repellendus minima!
+        Maxime, provident est.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt similique
+        molestias dolore, ab libero eius? Voluptatum quaerat architecto ex
+        blanditiis, perspiciatis dolor quisquam labore fuga repellendus minima!
+        Maxime, provident est.
+      </p>
+    </div>
+  </q-page>
 </template>
 
 <script>
-// import MapSetup from "./MapSetup.vue";
 import { defineComponent } from "vue";
-import axios from "axios";
-
-const cors = require("cors")({
-  origin: true,
-});
 
 export default defineComponent({
-  mounted() {
-    this.showLocationOnMap(position.coords.latitude, position.coords.longitude);
-  },
-  data() {
-    return {
-      latLoc: "",
-      lngLoc: "",
-      error: "",
-    };
-  },
-  // components: {
-  //   MapSetup,
-  // },
-  methods: {
-    findLocation() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            this.getAddressFrom(
-              position.coords.latitude,
-              position.coords.longitude
-            );
-          },
-          (error) => {
-            console.log(error.message);
-          }
-        );
-      } else {
-        console.log("Not supported");
-      }
-    },
-    showLocationOnMap(latitude, longitude) {
-      var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: new google.maps.LatLng(latitude, longitude),
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-      });
-
-      new google.maps.Marker({
-        position: new google.maps.LatLng(latitude, longitude),
-        map: map,
-      });
-    },
-    async getAddressFrom(lat, lng) {
-      await axios
-        .get(
-          "https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyBX343Nmh74V7B37a98q1pbtkqYfVt77XI"
-        )
-        .then((response) => {
-          if (response.data.error_messaage) {
-            return cors(request, response, () => {
-              response.status(200).send("No Results");
-            });
-          } else {
-            console.log(response);
-            // this.latLoc = response.data.results[0].LatLng.lat;
-            // this.lngLoc = response.data.results[0].LatLng.lng;
-          }
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
-    },
-  },
+  name: "ProfilePage",
+  components: {},
 });
 </script>
--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,32 +1,145 @@
 <template>
-  <q-page class="q-pa-lg">
-    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
-    <div class="text-body1">
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt similique
-        molestias dolore, ab libero eius? Voluptatum quaerat architecto ex
-        blanditiis, perspiciatis dolor quisquam labore fuga repellendus minima!
-        Maxime, provident est.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt similique
-        molestias dolore, ab libero eius? Voluptatum quaerat architecto ex
-        blanditiis, perspiciatis dolor quisquam labore fuga repellendus minima!
-        Maxime, provident est.
-      </p>
-      <MapSetup></MapSetup>
-    </div>
-  </q-page>
+  <div>
+    <q-page class="q-pa-lg">
+      <q-input
+        placeholder="Whats Popyn?"
+        class="q-ma-md"
+        outlined
+        rounded
+        dense
+      >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+      <div class="q-pa-md q-gutter-sm">
+        <q-btn
+          v-model="coordinates"
+          icon="location_on"
+          @click="locationButtonPressed"
+        />
+        <q-btn
+          class="my-button"
+          color="secondary"
+          text-color="white"
+          label="Nearby"
+          @click="findLocationsNearby"
+        />
+      </div>
+    </q-page>
+
+    <section id="map"></section>
+  </div>
 </template>
 
+<style>
+.my-button {
+  padding: 10px 20px;
+}
+</style>
+
 <script>
-import MapSetup from "./MapSetup.vue";
 import { defineComponent } from "vue";
+import axios from "axios";
+
+const cors = require("cors")({
+  origin: true,
+});
 
 export default defineComponent({
   name: "SearchPage",
-  components: {
-    MapSetup,
+  mounted() {
+    this.showLocationOnMap(latLoc, lngLoc);
+  },
+  data() {
+    return {
+      latLoc: "29.628825",
+      lngLoc: "-82.37875",
+      error: "",
+      places: [],
+    };
+  },
+  computed: {
+    coordinates() {
+      return `${this.latLoc},${this.lngLoc}`;
+    },
+  },
+  methods: {
+    locationButtonPressed() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            this.latLoc = position.coords.latitude;
+            this.lngLoc = position.coords.longitude;
+            console.log(this.latLoc);
+          },
+          (error) => {
+            console.log(error.message);
+          }
+        );
+      } else {
+        console.log("Not supported");
+      }
+    },
+    showLocationOnMap(latitude, longitude) {
+      var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: new google.maps.LatLng(latitude, longitude),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+      });
+
+      new google.maps.Marker({
+        position: new google.maps.LatLng(latitude, longitude),
+        map: map,
+      });
+    },
+    async findLocationsNearby() {
+      await axios
+        .get(
+          "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+            this.latLoc +
+            "," +
+            this.lngLoc +
+            "&key=AIzaSyBX343Nmh74V7B37a98q1pbtkqYfVt77XI"
+        )
+        .then((response) => {
+          if (response.data.error_messaage) {
+            return cors(request, response, () => {
+              response.status(200).send("No Results");
+            });
+          } else {
+            console.log(response.data.results[0]);
+            // this.latLoc = response.data.results[0].LatLng.lat;
+            // this.lngLoc = response.data.results[0].LatLng.lng;
+          }
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    },
+    // async findLocationsNearby() {
+    //   await axios
+    //     .get(
+    //       // `https://maps.googleapis.com/maps/api/place/nearbysearch/output?location=${this.latLoc},${this.lngLoc}&radius=50000&type=bar&type=night_club&key=AIzaSyBX343Nmh74V7B37a98q1pbtkqYfVt77XI`
+    //       "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
+    //         latLoc +
+    //         "," +
+    //         lngLoc +
+    //         "&radius=50000&type=bar&type=night_club&key=AIzaSyBX343Nmh74V7B37a98q1pbtkqYfVt77XI"
+    //     )
+    //     .then((response) => {
+    //       if (response.data.error_messaage) {
+    //         return cors(request, response, () => {
+    //           response.status(200).send("No Results");
+    //         });
+    //       } else {
+    //         console.log(response.data.results);
+    //       }
+    //     })
+    //     .catch((error) => {
+    //       console.log(error.message);
+    //     });
+    // },
   },
 });
 </script>
