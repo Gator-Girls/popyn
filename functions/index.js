97b7a7753200b787569b524f9eea93bad1ce2a8e
vale3760 <vale3760@gmail.com>
2023-04-04 22:42:02 -0400

    api working organized

--- functions/index.js
+++ functions/index.js
@@ -17,8 +17,21 @@ exports.findExactLocation = functions.https.onRequest((request, response) => {
       if (data.error_message) {
         response.status(200).send("No Results");
       } else {
-        let place = data.results[0];
-        response.status(200).send(place);
+        let placeResult = data.results[0];
+        // let imageUrl = null;
+        // if (placeResult.photos && placeResult.photos.length > 0) {
+        //   let photo = placeResult.photos[0];
+        //   imageUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo.photo_reference}&key=YOUR_GOOGLE_PLACES_API_KEY`;
+        //   // Now you can use the imageUrl to display the image in your application
+        // }
+        const placeObj = {
+          id: placeResult.place_id,
+          name: placeResult.name,
+          address: placeResult.vicinity,
+          // imageSrc: imageUrl,
+          rating: placeResult.rating,
+        };
+        response.status(200).send(placeObj);
       }
     } catch (error) {
       console.log(error);
@@ -27,23 +40,88 @@ exports.findExactLocation = functions.https.onRequest((request, response) => {
   });
 });
 
-// exports.findLocationsNearby = functions.https.onRequest((request, response) => {
-//   cors(request, response, async () => {
-//     try {
-//       let latlngLocation = encodeURI(request.body.latlng);
-//       console.log(latlngLocation);
-//       let { data } = await axios.get(
-//         "https://maps.googleapis.com/maps/api/geocode/json?latlng=29.652,-82.345&key=AIzaSyBX343Nmh74V7B37a98q1pbtkqYfVt77XI"
-//       );
-//       if (data.error_message) {
-//         response.status(200).send("No Results");
-//       } else {
-//         let place = data.results[0];
-//         response.status(200).send(place);
-//       }
-//     } catch (error) {
-//       console.log(error);
-//       response.status(500).send();
-//     }
-//   });
-// });
+exports.findNearbyBars = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+    try {
+      let latlngLocation = encodeURI(request.body.latlng);
+      functions.logger.log("latlngLocation:", latlngLocation);
+      // console.log(latlngLocation);
+      //console.log(this.$route.query.latlng);
+      let { data } = await axios.get(
+        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
+          latlngLocation +
+          "&radius=5000&type=bar&rankby=prominence&key=AIzaSyBX343Nmh74V7B37a98q1pbtkqYfVt77XI"
+      );
+      if (data.error_message) {
+        response.status(200).send("No Results");
+      } else {
+        let places = data.results;
+        let placeObj = [];
+        let placesAllObjs = [];
+        for (var i = 0; i < places.length; i++) {
+          // let imageUrl = null;
+          // if (places[i].photos && places[i].photos.length > 0) {
+          //   let photo = places[i].photos[0];
+          //   imageUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo.photo_reference}&key=YOUR_GOOGLE_PLACES_API_KEY`;
+          // }
+          placeObj = {
+            id: places[i].place_id,
+            name: places[i].name,
+            address: places[i].vicinity,
+            // imageSrc: imageUrl,
+            rating: places[i].rating,
+          };
+          placesAllObjs.push(placeObj);
+          placeObj = [];
+        }
+        response.status(200).send(placesAllObjs);
+      }
+    } catch (error) {
+      console.log(error);
+      response.status(500).send();
+    }
+  });
+});
+
+exports.findNearbyClubs = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+    try {
+      let latlngLocation = encodeURI(request.body.latlng);
+      functions.logger.log("latlngLocation:", latlngLocation);
+      // console.log(latlngLocation);
+      //console.log(this.$route.query.latlng);
+      let { data } = await axios.get(
+        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
+          latlngLocation +
+          "&radius=5000&type=night_club&rankby=prominence&key=AIzaSyBX343Nmh74V7B37a98q1pbtkqYfVt77XI"
+      );
+      if (data.error_message) {
+        response.status(200).send("No Results");
+      } else {
+        let places = data.results;
+        let placeObj = [];
+        let placesAllObjs = [];
+        for (var i = 0; i < places.length; i++) {
+          // let imageUrl = null;
+          // if (places[i].photos && places[i].photos.length > 0) {
+          //   let photo = places[i].photos[0];
+          //   imageUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo.photo_reference}&key=YOUR_GOOGLE_PLACES_API_KEY`;
+          // }
+          placeObj = {
+            id: places[i].place_id,
+            name: places[i].name,
+            address: places[i].vicinity,
+            // imageSrc: imageUrl,
+            rating: places[i].rating,
+          };
+          placesAllObjs.push(placeObj);
+          placeObj = [];
+        }
+        response.status(200).send(placesAllObjs);
+      }
+    } catch (error) {
+      console.log(error);
+      response.status(500).send();
+    }
+  });
+});
--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -4,6 +4,7 @@
       <q-input
         placeholder="Whats Popyn?"
         class="q-ma-md"
+        v-model="coordinates"
         outlined
         rounded
         dense
@@ -13,29 +14,48 @@
         </template>
       </q-input>
       <div class="q-pa-md q-gutter-sm">
-        <q-btn
-          v-model="coordinates"
-          icon="location_on"
-          @click="locationButtonPressed"
-        />
-        <q-btn
-          class="my-button"
-          color="primary"
-          text-color="white"
-          label="Exact"
-          @click="findExactLoc"
-        />
+        <q-btn icon="location_on" @click="findExactLoc" />
         <q-btn
           class="my-button"
           color="secondary"
           text-color="white"
-          label="Nearby"
-          @click="findExactLoc"
+          label="Bars"
+          @click="findBarsNearby"
+        />
+        <q-btn
+          class="my-button"
+          color="purple"
+          text-color="white"
+          label="Clubs"
+          @click="findClubsNearby"
         />
       </div>
-    </q-page>
 
-    <section id="map"></section>
+      <q-list>
+        <q-item v-for="place in places" :key="place.id">
+          <q-item-section>
+            <img :src="place.imageSrc" alt="Place image" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>
+              <div>{{ place.name }}</div>
+              <div>{{ place.address }}</div>
+              <div>
+                <q-rating
+                  v-model="place.rating"
+                  :max="5"
+                  :icon="'star'"
+                  :size="'sm'"
+                  :readonly="true"
+                  :color="'black'"
+                />
+              </div>
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-page>
   </div>
 </template>
 
@@ -59,12 +79,30 @@ export default defineComponent({
   //   this.showLocationOnMap(latLoc, lngLoc);
   // },
   data: () => ({
-    latLoc: "",
-    lngLoc: "",
-    error: "",
-    current_placeId: "",
-    address: "",
+    latLoc: "29.6532",
+    lngLoc: "-82.3449",
     places: [],
+    top_places: [
+      "ChIJoQC7ieGj6IgRhZVl0mVRx5c",
+      "ChIJfzM7wIyj6IgRBdNiPqiE4g0",
+      "ChIJwW45IY2j6IgR0obimGuW0Xo",
+      "ChIJwW45IY2j6IgR0obimGuW0Xo",
+      "ChIJ0bK0RUKj6IgRoMEfv3FqFR8",
+      "ChIJpRSBsY2j6IgRugXoTUGeRIc",
+      "ChIJXzqBxxSj6IgRU2IaUzNKHgc",
+      "ChIJkf0q7oyj6IgR7sFmwD_TSq8",
+      "ChIJ7cPd8oSj6IgRBdNVcm4dI7w",
+      "ChIJPam18Yyj6IgR_IB_s_lRHsc",
+      "ChIJF69YHY2j6IgR1eJB1R4qJXE",
+      "ChIJeWQGOY2j6IgRolwm3higq5w",
+      "ChIJcZacuIyj6IgRX_XTQhT5UoI",
+      "ChIJaXGagXij6IgRDp0ti8KfLBE",
+      "ChIJCURNZYKj6IgRX0Ismyjwfas",
+      "ChIJPS0RhHij6IgRYdUd0BbiSzo",
+      "ChIJj2W1eIKj6IgReGadK5Wxt58",
+      "ChIJ1TQsf4Kj6IgRrNWM6dG-KhI",
+      "ChIJc-28d4Kj6IgRO5rIx4Nw4Ss",
+    ],
   }),
   computed: {
     coordinates() {
@@ -72,24 +110,28 @@ export default defineComponent({
     },
   },
   methods: {
-    locationButtonPressed() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            this.latLoc = position.coords.latitude;
-            this.lngLoc = position.coords.longitude;
-            console.log("latitude" + this.latLoc);
-            console.log("longitude" + this.lngLoc);
-          },
-          (error) => {
-            console.log(error.message);
-          }
-        );
-      } else {
-        console.log("Not supported");
-      }
+    AccessLocation() {
+      this.latLoc = "29.6532";
+      this.lngLoc = "-82.3449";
+
+      // if (navigator.geolocation) {
+      //   navigator.geolocation.getCurrentPosition(
+      //     (position) => {
+      //       this.latLoc = position.coords.latitude;
+      //       this.lngLoc = position.coords.longitude;
+      //       console.log("latitude: " + this.latLoc);
+      //       console.log("longitude: " + this.lngLoc);
+      //     },
+      //     (error) => {
+      //       console.log(error.message);
+      //     }
+      //   );
+      // } else {
+      //   console.log("Not supported");
+      // }
     },
     async findExactLoc() {
+      this.AccessLocation();
       const latlng = `${this.latLoc},${this.lngLoc}`;
 
       let { data } = await axios.post(
@@ -100,34 +142,84 @@ export default defineComponent({
       );
 
       if (data === "No Results") {
-        // return cors(request, response, () => {
-        //   response.status(500).send();
-        // });
-        console.log(data);
-        console.log("here");
-        return;
+        return cors(request, response, () => {
+          response.status(500).send();
+        });
       }
+
+      let obj = {
+        id: data.id,
+        name: data.name,
+        address: data.address,
+        image: data.icon,
+        rating: data.rating,
+      };
+      this.places.push(obj);
 
       console.log(data);
     },
-    // async findLocNearby() {
-    //   let latlng = this.latLoc + "," + this.lngLoc;
-    //   let { data } = await axios.get(
-    //     "https://us-central1-popyn-18d51.cloudfunctions.net/findExactLocation",
-    //     {
-    //       latlng: latlng,
-    //     }
-    //   );
+    async findBarsNearby() {
+      this.places = [];
+      let latlngLocation = this.latLoc + "," + this.lngLoc;
+      let { data } = await axios.post(
+        "https://us-central1-popyn-18d51.cloudfunctions.net/findNearbyBars",
+        {
+          latlng: latlngLocation,
+        }
+      );
 
-    //   if (data === "No Results") {
-    //     return cors(request, response, () => {
-    //       console.log(error);
-    //       response.status(500).send();
-    //     });
-    //   }
+      if (data === "No Results") {
+        return cors(request, response, () => {
+          console.log(error);
+          response.status(500).send();
+        });
+      }
+      for (var i = 0; i < data.length; i++) {
+        let obj = {
+          id: data[i].id,
+          name: data[i].name,
+          address: data[i].address,
+          image: data[i].icon,
+          rating: data[i].rating,
+        };
+        this.places.push(obj);
+      }
 
-    //   console.log(data);
-    // },
+      // for (let i = 0; i < this.places.length; i++) {
+      //   console.log(this.places[i]);
+      // }
+      console.log("Bars");
+      console.log(data);
+    },
+    async findClubsNearby() {
+      let latlng = this.latLoc + "," + this.lngLoc;
+      let { data } = await axios.post(
+        "https://us-central1-popyn-18d51.cloudfunctions.net/findNearbyClubs",
+        {
+          latlng: latlng,
+        }
+      );
+
+      if (data === "No Results") {
+        return cors(request, response, () => {
+          console.log(error);
+          response.status(500).send();
+        });
+      }
+
+      for (var i = 0; i < data.length; i++) {
+        let obj = {
+          id: data[i].id,
+          name: data[i].name,
+          address: data[i].address,
+          image: data[i].icon,
+          rating: data[i].rating,
+        };
+        this.places.push(obj);
+      }
+      console.log("Clubs");
+      console.log(data);
+    },
   },
 });
 </script>
