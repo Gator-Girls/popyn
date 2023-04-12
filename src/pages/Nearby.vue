8ec7f41e46fb5b47c2f921cab6caad75274f364f
yelainelinares <yelainelinares@ufl.edu>
2023-04-12 18:25:33 -0400

    create pop, fixed login/signup, lists for bars

--- src/pages/Nearby.vue
+++ src/pages/Nearby.vue
@@ -7,104 +7,42 @@
       </q-tabs>
     </div>
 
+    <!-- <img
+      alt="Popyn logo"
+      src="~assets/Popyn_-logos_transparent.ico"
+      style="width: 300px; height: 300px"
+    /> -->
+
     <div class="column items-center">
-      <div class="row">
-        <q-btn
-          color="secondary text-black"
-          class="q-mt-md"
-          no-caps
-          style="width: 400px; height: 120px"
-        >
-          <div class="col-8 text-left">
-            <div class="text-weight-bold">1. Downtown Fats</div>
-            <div class="q-pl-sm">
-              <div>2.4 mi • Bar • $</div>
-              <div>Open 4:00pm - 2:00am</div>
-              <div>★★★★☆ (29)</div>
+      <q-item v-for="barP in bars" :key="barP.id" class="barP q-py-xs">
+        <div class="row">
+          <q-btn
+            color="secondary text-black"
+            class="q-mt-md"
+            no-caps
+            style="width: 400px; height: 120px"
+            to="/BackyardReviews"
+          >
+            <div class="col-8 text-left">
+              <div class="text-weight-bold">
+                {{ barP.ranking }}{{ barP.locName }}
+              </div>
+              <div class="q-pl-sm">
+                <div>{{ barP.distanceLine }}</div>
+                <div>{{ barP.hours }}</div>
+                <div>{{ barP.stars }}</div>
+              </div>
             </div>
-          </div>
-          <div class="col-4 self-center q-mt-sm">
-            <img
-              alt="Popyn logo"
-              src="~assets/dtf.ico"
-              style="width: 88px; height: 88px"
-            />
-          </div>
-        </q-btn>
-      </div>
-      <div class="row">
-        <q-btn
-          color="secondary text-black"
-          class="q-mt-md"
-          no-caps
-          style="width: 400px; height: 120px"
-          to="/BackyardReviews"
-        >
-          <div class="col-8 text-left">
-            <div class="text-weight-bold">2. The Backyard at Boca Fiesta</div>
-            <div class="q-pl-sm">
-              <div>2.7 mi • Bar • $$</div>
-              <div>Open 4:00pm - 2:00am</div>
-              <div>★★★★☆ (38)</div>
+            <div class="col-4 self-center q-mt-sm">
+              <img
+                alt="Popyn logo"
+                :src="barP.img"
+                style="width: 88px; height: 88px"
+              />
             </div>
-          </div>
-          <div class="col-4 self-center q-mt-sm">
-            <img
-              alt="Popyn logo"
-              src="~assets/backyard.ico"
-              style="width: 88px; height: 88px"
-            />
-          </div>
-        </q-btn>
-      </div>
-      <div class="row">
-        <q-btn
-          color="secondary text-black"
-          class="q-mt-md"
-          no-caps
-          style="width: 400px; height: 120px"
-        >
-          <div class="col-8 text-left">
-            <div class="text-weight-bold">3. Arcade Bar</div>
-            <div class="q-pl-sm">
-              <div>2.8 mi • Bar • $</div>
-              <div>Open 4:00pm - 3:00am</div>
-              <div>★★★☆☆ (14)</div>
-            </div>
-          </div>
-          <div class="col-4 self-center q-mt-sm">
-            <img
-              alt="Popyn logo"
-              src="~assets/barcade.ico"
-              style="width: 88px; height: 88px"
-            />
-          </div>
-        </q-btn>
-      </div>
-      <div class="row">
-        <q-btn
-          color="secondary text-black"
-          class="q-mt-md"
-          no-caps
-          style="width: 400px; height: 120px"
-        >
-          <div class="col-8 text-left">
-            <div class="text-weight-bold">4. Fat Daddy's</div>
-            <div class="q-pl-sm">
-              <div>3.1 mi • Bar • $</div>
-              <div>Open 4:00pm - 2:00am</div>
-              <div>★★★☆☆ (20)</div>
-            </div>
-          </div>
-          <div class="col-4 self-center q-mt-xs">
-            <img
-              alt="Popyn logo"
-              src="~assets/fatdaddy.ico"
-              style="width: 88px; height: 88px"
-            />
-          </div>
-        </q-btn>
-      </div>
+          </q-btn>
+        </div>
+      </q-item>
     </div>
   </q-page>
 </template>
@@ -113,6 +51,44 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "PageNearby",
+  name: "PageTrending",
+  data() {
+    return {
+      bars: [
+        {
+          locName: "Downtown Fats",
+          ranking: "1. ",
+          distanceLine: "2.4 mi • Bar • $",
+          hours: "Open 4:00pm - 2:00am",
+          stars: "★★★★☆ (29)",
+          img: require("../assets/dtf.ico"),
+        },
+        {
+          locName: "The Backyard at Boca Fiesta",
+          ranking: "2. ",
+          distanceLine: "2.7 mi • Bar • $$",
+          hours: "Open 4:00pm - 2:00am",
+          stars: "★★★★☆ (38)",
+          img: require("../assets/backyard.ico"),
+        },
+        {
+          locName: "Arcade Bar",
+          ranking: "3. ",
+          distanceLine: "2.8 mi • Bar • $",
+          hours: "Open 4:00pm - 3:00am",
+          stars: "★★★☆☆ (14)",
+          img: require("../assets/barcade.ico"),
+        },
+        {
+          locName: "Fat Daddy's",
+          ranking: "4. ",
+          distanceLine: "3.1 mi • Bar • $",
+          hours: "Open 4:00pm - 2:00am",
+          stars: "★★★☆☆ (20)",
+          img: require("../assets/fatdaddy.ico"),
+        },
+      ],
+    };
+  },
 });
 </script>
