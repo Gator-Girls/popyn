9a8b69fd304708254c90b6edb5d2534886c0c1ef
vale3760 <vale3760@gmail.com>
2023-04-13 10:51:57 -0400

    map now has places
    check only mostly MapContainer page and maybe add it to the maps page in mkelly

--- src/main.js
+++ src/main.js
@@ -1,31 +1,63 @@
-import Vue from "vue";
-import * as VueGoogleMaps from "vue2-google-maps";
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: "AIzaSyBX343Nmh74V7B37a98q1pbtkqYfVt77XI",
-  },
-});
+// import Vue from "vue";
+// import * as VueGoogleMaps from "vue2-google-maps";
+// Vue.use(VueGoogleMaps, {
+//   load: {
+//     key: "AIzaSyBX343Nmh74V7B37a98q1pbtkqYfVt77XI",
+//   },
+// });
 
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+// import firebase from "firebase/app";
+// import "firebase/auth";
+// import "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCdMwTwN5wmPFtBCnRGtE0OYixf-jJLK9k",
-  authDomain: "popyn-18d51.firebaseapp.com",
-  projectId: "popyn-18d51",
-  storageBucket: "popyn-18d51.appspot.com",
-  messagingSenderId: "699103021609",
-  appId: "1:699103021609:web:dbd96da9c777eaf541aa38",
-  measurementId: "G-9LSB6DSJJ8",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCdMwTwN5wmPFtBCnRGtE0OYixf-jJLK9k",
+//   authDomain: "popyn-18d51.firebaseapp.com",
+//   projectId: "popyn-18d51",
+//   storageBucket: "popyn-18d51.appspot.com",
+//   messagingSenderId: "699103021609",
+//   appId: "1:699103021609:web:dbd96da9c777eaf541aa38",
+//   measurementId: "G-9LSB6DSJJ8",
+// };
 
-firebase.initializeApp(firebaseConfig);
-const Firestore = firebase.firestore;
-export const db = Firebase();
+// firebase.initializeApp(firebaseConfig);
+// const Firestore = firebase.firestore();
+// export const db = Firestore;
 
-Vue.config.productionTip = false;
+// Vue.config.productionTip = false;
 
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+// new Vue({
+//   render: (h) => h(App),
+// }).$mount("#app");
+
+// import Vue from "vue";
+// import * as VueGoogleMaps from "vue2-google-maps";
+// Vue.use(VueGoogleMaps, {
+//   load: {
+//     key: "AIzaSyBX343Nmh74V7B37a98q1pbtkqYfVt77XI",
+//   },
+// });
+
+// import firebase from "firebase/app";
+// import "firebase/auth";
+// import "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCdMwTwN5wmPFtBCnRGtE0OYixf-jJLK9k",
+//   authDomain: "popyn-18d51.firebaseapp.com",
+//   projectId: "popyn-18d51",
+//   storageBucket: "popyn-18d51.appspot.com",
+//   messagingSenderId: "699103021609",
+//   appId: "1:699103021609:web:dbd96da9c777eaf541aa38",
+//   measurementId: "G-9LSB6DSJJ8",
+// };
+
+// firebase.initializeApp(firebaseConfig);
+// const Firestore = firebase.firestore;
+// export const db = Firebase();
+
+// Vue.config.productionTip = false;
+
+// new Vue({
+//   render: (h) => h(App),
+// }).$mount("#app");
--- src/pages/MapContainer.vue
+++ src/pages/MapContainer.vue
@@ -5,8 +5,51 @@
 </template>
 
 <script>
+// import { db } from "./main";
 export default {
   name: "MapContainer",
+  data() {
+    return {
+      places: [
+        {
+          name: "Arcade Bar",
+          lat: 29.6521194,
+          lng: -82.3247243,
+          address: "6 E University Ave, Gainesville, FL 32601, USA",
+        },
+        {
+          name: "JJ's Tavern",
+          lat: 29.6524179,
+          lng: -82.3463849302915,
+          address: "1702 W University Ave Suite G, Gainesville, FL 32603, USA",
+        },
+        {
+          name: "High Dive",
+          lat: 29.6504336,
+          lng: -82.32697979999999,
+          address: "210 SW 2nd Ave, Gainesville, FL 32601, USA",
+        },
+        {
+          name: "The Social at Midtown",
+          lat: 29.6525821,
+          lng: -82.34561149999999,
+          address: "1728 W University Ave, Gainesville, FL 32603, USA",
+        },
+        {
+          name: "The Backyard at Boca Fiesta & Palomino",
+          lat: 29.6494382,
+          lng: -82.32434510000002,
+          address: "232 1/2, SE 1st St, Gainesville, FL 32601, USA",
+        },
+        {
+          name: "FAT DADDY'S",
+          lat: 29.6524179,
+          lng: -82.345129,
+          address: "1702 W University Ave Suite A1, Gainesville, FL 32603, USA",
+        },
+      ],
+    };
+  },
   mounted() {
     // Load the Google Maps API
     const script = document.createElement("script");
@@ -16,15 +59,25 @@ export default {
   },
   methods: {
     initMap() {
-      const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 37.7749, lng: -122.4194 }, // set the initial center of the map
-        zoom: 12, // set the initial zoom level of the map
+      var map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 29.65322, lng: -82.3449 }, // set the initial center of the map
+        zoom: 14, // set the initial zoom level of the map
       });
       // add markers to the map
-      const marker = new google.maps.Marker({
-        position: { lat: 37.7749, lng: -122.4194 },
-        map: map,
-        title: "San Francisco",
+      var infowindow = new google.maps.InfoWindow();
+      this.places.forEach((place) => {
+        let marker = new google.maps.Marker({
+          position: { lat: place.lat, lng: place.lng },
+          map: map,
+          title: place.name,
+        });
+        google.maps.event.addListener(marker, "click", () => {
+          infowindow.setContent(`<div>
+                                    <h3>${place.name}</h3>
+                                    <p>${place.address}</p>
+                                  </div>`);
+          infowindow.open(map, marker);
+        });
       });
     },
   },
@@ -33,7 +86,7 @@ export default {
 
 <style scoped>
 .map-container {
-  height: 500px;
+  height: 1000px;
 }
 
 .map {
